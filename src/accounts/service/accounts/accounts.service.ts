255c2ad1765ddcdced848bd761adb858e0ea2328
vuhiepaso <vuhiepomg@gamail.com>
2023-02-28 17:24:34 +0700

    update

--- src/accounts/service/accounts/accounts.service.ts
+++ src/accounts/service/accounts/accounts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Account } from 'src/entity/account';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AccountsService {
   constructor(
@@ -11,14 +12,18 @@ export class AccountsService {
     return await this.accountRepository.find();
   }
   async createAccounts(data): Promise<Account> {
-    // const pass = this.hashPassword
+    const password = await this.hashPassword(data.password);
     const account = new Account();
     account.id;
     account.email = data.email;
     account.username = data.username;
-    account.password = data.password;
+    account.password = password;
     account.isActive = data.isActive;
 
-    return await this.accountRepository.save(data);
+    return await this.accountRepository.save(account);
+  }
+  async hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(password, salt);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AppService {
-  async hashPassword(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt();
-    return bcrypt.hash(password, salt);
+  getHello(): string {
+    return 'Hello World!sssssssssssssaaaaa';
+  }
+  getName(): object {
+    return {
+      name: 'hiep',
+    };
   }
 }
