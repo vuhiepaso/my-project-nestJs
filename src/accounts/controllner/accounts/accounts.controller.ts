5ce0aa9b7f85b95e204c5aa6080021c984813337
vuhiepaso <vuhiepomg@gamail.com>
2023-03-02 17:36:03 +0700

    update

--- src/accounts/controllner/accounts/accounts.controller.ts
+++ src/accounts/controllner/accounts/accounts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Response } from '@nestjs/common';
 import { AccountsService } from 'src/accounts/service/accounts/accounts.service';
 import { Account } from 'src/entity/account';
 
@@ -7,12 +7,37 @@ export class AccountsController {
   constructor(private accountsService: AccountsService) {}
 
   @Get()
-  getAccounts(): Promise<Account[]> {
-    return this.accountsService.findAccounts();
+  async getAccounts(@Response() res): Promise<Account[]> {
+    try {
+      const accounts = await this.accountsService.findAccounts();
+      return res.status(200).json({
+        status: 'success',
+        message: 'Get accounts successfully!',
+        data: accounts,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Error getting users!',
+        error: error,
+      });
+    }
   }
 
   @Post('add')
-  createAccounts(@Body() account: Account) {
-    this.accountsService.createAccounts(account);
+  async createAccounts(@Body() account: Account, @Response() res) {
+    try {
+      await this.accountsService.createAccounts(account);
+      return res.status(200).json({
+        status: 'success',
+        message: 'Add accounts successfully!',
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'Error getting users!',
+        error: error,
+      });
+    }
   }
 }
