f29a731e5171efd2fccaf4e5befec426afe5f50d
vuhiepaso <vuhiepomg@gamail.com>
2023-04-03 17:34:51 +0700

    update

--- src/equipment/controller/equipment/equipment.controller.ts
+++ src/equipment/controller/equipment/equipment.controller.ts
@@ -6,6 +6,8 @@ import {
   Delete,
   Body,
   Response,
+  Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 
 import { AuthToken } from 'common/decorator/validate.token';
@@ -18,33 +20,95 @@ export class EquipmentController {
 
   //CREATE
   @Post()
-  createEquipment(
-    @AuthToken([ADMIN]) payload: any,
+  async createEquipment(
+    @AuthToken([ADMIN]) userInfo: any,
     @Body() equipment: Equipment1,
     @Response() res: any,
   ) {
     const data = new Equipment1();
-    data.id_user = payload.id;
+    data.id_user = userInfo.id;
     data.name = equipment.name;
-    this.equipmentService.createEquipment(data);
-    return res.status(200).json({
-      data,
-      payload,
-    });
+    try {
+      const dataReq = await this.equipmentService.createEquipment(data);
+      return res.status(200).json({
+        status: 200,
+        message: 'Create successfully',
+        data: dataReq,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error,
+        message: 'Create fail',
+      });
+    }
   }
   //READING
   @Get('/list')
-  listEquipment(@AuthToken([ADMIN, USER]) payload: any) {
-    console.log('payload', payload);
+  async listEquipment(
+    @AuthToken([ADMIN, USER]) userInfo: any,
+    @Response() res: any,
+  ) {
+    try {
+      const data = await this.equipmentService.listEquipment(userInfo.id);
+      return res.status(200).json({
+        status: 200,
+        message: 'Request successfully',
+        data: data,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error,
+        message: 'Request fail',
+      });
+    }
   }
   //UPDATE
-  @Patch('/update')
-  updateEquipment(@AuthToken([ADMIN, USER]) payload: any) {
-    console.log('payload', payload);
+  @Patch('/update/:id')
+  async updateEquipment(
+    @AuthToken([ADMIN, USER]) userInfo: any,
+    @Body() equipment: Equipment1,
+    @Param('id', ParseIntPipe) id: number,
+    @Response() res: any,
+  ) {
+    try {
+      const data = new Equipment1();
+      data.id = id;
+      data.status = equipment.status;
+      if (userInfo.role === ADMIN) {
+        data.name = equipment.name;
+        data.isActive = equipment.isActive;
+      }
+      const dataRq = await this.equipmentService.updateEquipment(data);
+      return res.status(200).json({
+        dataRq,
+        status: 200,
+        message: 'Request successfully',
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error,
+        message: 'Request fail',
+      });
+    }
   }
   //DELETE
-  @Delete('/delete')
-  deleteEquipment(@AuthToken([ADMIN]) payload: any) {
-    console.log('payload', payload);
+  @Delete('/delete/:id')
+  async deleteEquipment(
+    @AuthToken([ADMIN]) userInfo: any,
+    @Param('id') id: number,
+    @Response() res: any,
+  ) {
+    try {
+      await this.equipmentService.deleteEquipment(id);
+      return res.status(200).json({
+        status: 200,
+        message: 'Request successfully',
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error,
+        message: 'Request fail',
+      });
+    }
   }
 }
--- src/equipment/service/equipment/equipment.service.ts
+++ src/equipment/service/equipment/equipment.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Equipment1 } from 'entity/equipment1';
 import { Repository } from 'typeorm';
+import { Equal } from 'typeorm';
+
 @Injectable()
 export class EquipmentService {
   constructor(
@@ -9,16 +11,44 @@ export class EquipmentService {
     private equipmentRepository: Repository<Equipment1>,
   ) {}
 
+  // CREATE EQUIPMENT
   async createEquipment(data: Equipment1) {
     return await this.equipmentRepository.save(data);
   }
-  listEquipment() {
-    console.log('ok');
+
+  // LIST EQUIPMENT
+  async listEquipment(userId: number) {
+    return await this.equipmentRepository.findBy({
+      id_user: Equal(userId),
+    });
   }
-  updateEquipment() {
-    console.log('ok');
+
+  // UPDATE EQUIPMENT
+  async updateEquipment(equipment: Equipment1) {
+    let foundEquipment = await this.findEquipment(equipment.id);
+    foundEquipment = { ...foundEquipment, ...equipment };
+    console.log('abc', foundEquipment);
+    // return await this.equipmentRepository.save(foundEquipment);
+    return equipment;
   }
-  deleteEquipment() {
-    console.log('ok');
+
+  // DELETE EQUIPMENT
+  async deleteEquipment(id: number) {
+    await this.findEquipment(id);
+    return await this.equipmentRepository.softDelete(id);
+  }
+
+  // FIND ONE BY ID
+  async findEquipment(id: number): Promise<Equipment1> {
+    const foundEquipment = await this.equipmentRepository.findOneBy({
+      id: id,
+    });
+    if (!foundEquipment) {
+      throw new HttpException(
+        `Equipment with id ${id} not found.`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return foundEquipment;
   }
 }
