8e666f59426790e38900cdb7b187324e917f418a
vuhiepaso <vuhiepomg@gamail.com>
2023-04-17 17:36:43 +0700

    update

--- src/gateway/events.gateway.ts
+++ src/gateway/events.gateway.ts
@@ -1,12 +1,28 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 @WebSocketGateway(8001)
-export class EventsGateway {
+export class EventsGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('connection', socket.id);
+    });
+  }
+
   @SubscribeMessage('events')
   onEvent(@MessageBody() body: any) {
-    console.log(body);
+    console.log('onEvent', body);
+    this.server.emit('onEvents', {
+      msg: 'on events',
+      content: body,
+    });
   }
 }
